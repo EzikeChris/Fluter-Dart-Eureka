de8e36997f88e4458c30d0dc4401a51eaccb7451
ezikechris <christophermbe1009@gmail.com>
2021-03-01 21:44:16 +0100

    Initial Commit

---
+++ android/app/src/main/kotlin/christopherezike/alarm_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package christopherezike.alarm_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// The starting point for all apps
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );

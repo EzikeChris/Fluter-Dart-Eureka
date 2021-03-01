36f583a6805128c927acfd9bdf5143785ef9e11f
ezikechris <christophermbe1009@gmail.com>
2021-03-02 00:16:15 +0100

    add Scafold appBar Body Backgroundcolor image

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-// The starting point for all apps
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+//The starting point for all apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Alarm App'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor: Colors.blueAccent,
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://i.fbcd.co/products/original/8818e54ec2f94c0b9d8b3c5c94c4ec818c28784e38c0e97c739b966ba549f61f.jpg'),
+            ),
+          )),
+    ),
+  );
+}
